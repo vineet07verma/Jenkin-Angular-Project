a1b4d48d098f78fa530be05d10fb768978d3efd6
VINEET VERMA <vineet07verma@gmail.com>
2018-04-11 18:31:44 +0530

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,3 +64,9 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+
+import 'intl';
+
+import 'intl/locale-data/jsonp/en';
+
